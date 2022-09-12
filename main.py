32d90b061d68d6db674aaefae4f3bc1367cdf808
IlPalazz <fede.palazzi1999@gmail.com>
2022-09-12 17:18:27 +0200

    Implemented basic exchange of random machine data

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ from time import sleep
 from confluent_kafka import Consumer
 import threading
 from time import sleep
-import assets.helloworld_pb2 as HelloWorld
+#import assets.helloworld_pb2 as HelloWorld
+import assets.machine_data_pb2 as MachineData
 
 
 def checkMessages(topic, app):
@@ -36,11 +37,13 @@ def checkMessages(topic, app):
                 print("ERROR: %s".format(msg.error()))
             else:
                 print('Received: ' + str(msg.value()))
-                hello_proto = HelloWorld.HelloMessage()
-                hello_proto.ParseFromString(msg.value())
-                print("Name: " + hello_proto.name)
+                machine_data = MachineData.MachineDataMessage()
+                machine_data.ParseFromString(msg.value())
+                print("Temp: " + str(machine_data.temp))
+                print("Power: " + str(machine_data.power))
+                print("Alarm: " + str(machine_data.alarm))
                 # Update UI
-                app.updateMachineData(hello_proto)
+                app.updateMachineData(machine_data)
 
             sleep(0.1)
 
@@ -61,7 +64,7 @@ class OperatorHMI(Tk):
         # configure the root window
         self.title('Operator HMI')
         self.resizable(0, 0)
-        self.geometry('600x500')
+        self.geometry('700x600')
         # Set default style
         self.style = ttk.Style(self)
         # Create style used by default for all Frames
@@ -74,23 +77,43 @@ class OperatorHMI(Tk):
         self.rowconfigure(0, weight=1)
 
         # Title label
-        self.title_lbl = ttk.Label(self.mainframe, text="Operator HMI")
-        self.title_lbl.grid(column=0, row=0, sticky=(N, W))
+        self.title_lbl = ttk.Label(
+            self.mainframe, text="Operator HMI", font=("Arial", 25))
+        self.title_lbl.grid(column=0, row=0, sticky=(N, W), pady=(10, 30))
 
         # Img label
         # Create an object of tkinter ImageTk
         self.img = ImageTk.PhotoImage(Image.open(self.img_path))
         # Create a Label Widget to display the text or Image
-        self.img_lbl = ttk.Label(self.mainframe, image=self.img)
-        self.img_lbl.grid(column=0, row=1, sticky=(N, W))
+        self.img_lbl = ttk.Label(self.mainframe, image=self.img, width=80)
+        self.img_lbl.grid(column=0, row=1, sticky=(N, W), pady=(5, 25))
+
+        # Temp label
+        self.temp_lbl = ttk.Label(
+            self.mainframe, text="Temp: [°C]", font=("Arial", 14))
+        self.temp_lbl.grid(column=0, row=2, sticky=(N, W),
+                           pady=(10, 10), padx=10)
+
+        # Power label
+        self.power_lbl = ttk.Label(
+            self.mainframe, text="Power: [W]", font=("Arial", 14))
+        self.power_lbl.grid(column=0, row=3, sticky=(N, W),
+                            pady=(10, 10), padx=10)
+
+        # Alarm label
+        self.alarm_lbl = ttk.Label(
+            self.mainframe, text="Alarm: ", font=("Arial", 14))
+        self.alarm_lbl.grid(column=0, row=4, sticky=(N, W),
+                            pady=(10, 10), padx=10)
 
     def updateMachineData(self, data):
-        self.title_lbl["text"] = data.name
+        self.temp_lbl["text"] = f"Temp: {str(round(data.temp, 2))} [°C]"
+        self.power_lbl["text"] = f"Power: {str(round(data.power, 2))} [W]"
+        self.alarm_lbl["text"] = f"Alarm: {str(data.alarm)}"
 
 
 if __name__ == '__main__':
     app = OperatorHMI()
-    # root.after(500, checkMessages, root)
     t = threading.Thread(target=checkMessages, args=("teaming_event", app,))
     t.start()
     app.mainloop()
--- producer_proto.py
+++ producer_proto.py
@@ -2,11 +2,25 @@
 
 from random import choice
 from confluent_kafka import Producer
-import assets.helloworld_pb2 as HelloWorld
+#import assets.helloworld_pb2 as HelloWorld
+import assets.machine_data_pb2 as MachineData
 from time import sleep
+import random
+
+
+def getRandomTemp():
+    return round((random.random()*100+10), 2)
+
+
+def getRandomPower():
+    return round((random.random()*1000+100), 2)
+
+
+def getRandomAlarm():
+    return bool(random.getrandbits(1))
+
 
 if __name__ == '__main__':
-
     config = {'bootstrap.servers': 'localhost:9092'}
 
     # Create Producer instance
@@ -19,8 +33,8 @@ if __name__ == '__main__':
         if err:
             print('ERROR: Message failed delivery: {}'.format(err))
         else:
-            print("Produced event to topic {topic}: name = {name}".format(
-                topic=msg.topic(), name=msg.value()))
+            print("Produced event to topic {topic}: data = {data}".format(
+                topic=msg.topic(), data=msg.value()))
 
     # Produce data by selecting random values from these lists.
     topic = "teaming_event"
@@ -28,10 +42,13 @@ if __name__ == '__main__':
              'jbernard', 'htanaka', 'awalther']
 
     count = 0
-    for _ in range(10):
-        rand_name = choice(names)
-        hello_proto = HelloWorld.HelloMessage(name=rand_name)
-        serialized = hello_proto.SerializeToString()
+    for _ in range(100):
+        #rand_name = choice(names)
+        machine_data = MachineData.MachineDataMessage(
+            temp=getRandomTemp(),
+            power=getRandomPower(),
+            alarm=getRandomAlarm())
+        serialized = machine_data.SerializeToString()
         # produce(topic, value, key, callback)
         # producer.produce(topic, product, user_id, callback=delivery_callback)
         producer.produce(topic=topic, value=serialized,
